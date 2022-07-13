c3e4b901b495aae107e0bfda3b32ecd85735f14e
Uriel Aguirre <uriiaguirre11@outlook.com>
2022-07-13 18:09:05 -0300

    rutas

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const port = 3030;
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+app.get('/login.html', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
+app.get('/productCart.html', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productCart.html')))
+app.get('/productDetail.html', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productDetail.html')))
+app.get('/register.html', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')))
 
 app.listen(port, ()=> console.log('Server running in http://localhost:' + port));
