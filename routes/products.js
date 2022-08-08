b96ee639d14dbf4738b3a10d73d0b765db837137
FacundoOrcajo <facundo20orcajo@gmail.com>
2022-08-08 20:32:50 -0300

    Agregué metodos en los archivos de routes

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const {productCart, productDetail} = require('../controllers/productsController');
 
 /* products*/
-router.get('/', );
+router.get('/productCart', productCart);
+router.get('/productDetail', productDetail);
 
 module.exports = router;
