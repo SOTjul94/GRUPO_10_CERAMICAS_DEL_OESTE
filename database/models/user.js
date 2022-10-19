c74208864a76d527c74f356681e1f2f312ee4aaa
abrilbril <abrilbrilnu@gmail.com>
2022-10-19 18:41:17 -0300

    merge models, migrations and relations

--- database/models/user.js
+++ database/models/user.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-module.exports = (sequelize, dataTypes) => {
-    let alias = 'User';
-    let cols = {
-        idusers : {
-            type: dataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false
-        },
-        firstname : {
-            type: dataTypes.STRING(45),
-            allowNull:false
-        },
-        lastname : {
-            type: dataTypes.STRING(45),
-            allowNull:false
-        },
-        email : {
-            type: dataTypes.STRING(45),
-            allowNull:false
-        },
-        avatar_id : {
-            type: dataTypes.INTEGER,
-            primaryKey: true
-        },
-        document : {
-            type: dataTypes.DECIMAL,
-            primaryKey: true
-        },
-        nacionality : {
-            type: dataTypes.STRING(45),
-            allowNull:false
-        },
-        birthday : {
-            type: dataTypes.DATE,
-            allowNull: false
-        },
-        rol_id : {
-            type: dataTypes.INTEGER,
-            allowNull: false
-        },
-        password : {
-            type: dataTypes.STRING(255),
-            allowNull:false
-        }
-    };
-    let config = {
-        timestamps : true
-=======
 'use strict';
 const {
   Model
@@ -60,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
->>>>>>> Abril
     }
   }
   User.init({
