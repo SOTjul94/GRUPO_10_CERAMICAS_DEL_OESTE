b59a5a43431b20aae361ac98cd165e160ba3a03f
Eric <menaericdaniel@gmail.com>
2022-11-09 23:09:33 -0300

    get products done!

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,220 +1,139 @@
-const db = require('../database/models');
+const db = require("../database/models");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-const {loadProducts,storeProducts} = require('../data/productsModule');
-const { promiseImpl } = require('ejs');
-
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
-	search : (req, res) => {
-		const {keywords} = req.query;
+	totalProducts: (req, res) => {
 		db.Product.findAll({
+			include: ["images"],
+		})
+			.then((products) => {
+				return res.render("totalProducts", {
+					title: "totalProducts",
+					products,
+				});
+			})
+			.catch((error) => console.log(error));
+	},
+	filterProducts : (req,res) => {
+		db.Product.findAll({
+			include : ['images'],
 			where : {
-				[Op.or] : [
-					{
-						name : {
-							[Op.substring] : keywords
-						}
-					},
-					{
-						description : {
-							[Op.substring] : keywords
-						}
-					}
-				]
-			},
-			include : ['image']
-		})
-		.then(products => {
-			returnres.render('results',{
-				products,
-				keywords,
-				toThousand
-			})
-		})
-		.catch(error => console.log(error))
-	},
-    productCart : (req, res) => {
-        return res.render('productCart', {
-            title : 'ProductCart'
-        })
-    },
-    productDetail: (req, res) => {
-		/*const products = loadProducts();
-		const product = products.find(product => product.id === +req.params.id);
-		return res.render('productDetail',{
-			product,
-			toThousand
-		}) */
-		db.Product.findByPk(req.params.id)
-		  .then(product => res.render('productDetail',{
-			product,
-			toThousand
-		}))
-		.catch(error => console.log(error))
-	},
-    creationProduct : (req, res) => {
-		/*const products = loadProducts();
-		
-        return res.render('creationProduct', {
-            title : 'creationProduct',
-			products,
-			toThousand
-        }) */
-		db.Category.findAll({
-			attributes : ['id', 'name']
-		})
-		.then(categories => {
-			return res.render('creationProduct',{
-				categories
-			})
-		})
-		.catch(error => console.log(error))
-    },
-    totalProducts : (req, res) => {
-		const products = loadProducts();
-        return res.render('totalProducts', {
-            title : 'totalProducts',
-			products
-        })
-    },
-    editionProduct: (req, res) => {
-		let categories = db.Category.findAll({
-			attributes : ['id', 'name'],
-			order : ['name']
-		});
-		let product = db.Product.findByPk(req.params.id);
-		promiseImpl.all(([categories, product]))
-		.then(([categories, product]) => {
-			return res.render('editionProduct',{
-				product,
-				categories
-			})
-		})
-		.catch(error => console.log(error));
-		/*const products = loadProducts();
-		const product = products.find(product => product.id === +req.params.id);
-		res.render('editionProduct',{
-			product,
-			toThousand
-		})*/
-	},	
-	update: (req, res) => {
-		
-		/*const products = loadProducts();
-		const {name, price, code, color, description,caja, finish, style, dimensions, pei, transit, recomendation, espesor, model, form, origin} = req.body;
-		const productModify = products.map(product => {
-			if(product.id === +req.params.id){
-				return{
-					...product,
-					name : name?.trim(),
-					price : +price,
-					code : +code,
-					caja: +caja,
-					color: color?.trim(),
-					finish: finish?.trim(),
-					style: style?.trim(),
-					dimensions : +dimensions,
-					pei: +pei,
-					transit: transit?.trim(),
-					recomendation: recomendation?.trim(),
-					espesor: +espesor,
-					model: model?.trim(),
-					form: form?.trim(),
-					origin: origin?.trim(),
-					description : description?.trim(),
-					
-				}
+				category : req.query.category
 			}
-			return product
 		})
-		storeProducts(productModify) */
+			.then(products => {
+				return res.render("totalProducts", {
+					title: "totalProducts",
+					products,
+				});
+			})
+			.catch(error => console.log(error))
+	},
+	productDetail: (req, res) => {
+		db.Product.findByPk(req.params.id)
+			.then((product) =>
+				res.render("productDetail", {
+					product,
+					toThousand,
+				})
+			)
+			.catch((error) => console.log(error));
+	},
+	creationProduct: (req, res) => {
+		return res.render("creationProduct");
+	},
+	store: (req, res) => {
+		db.product
+			.create({
+				...req.body,
+				name: req.body.trim(),
+				description: req.body.description.trim(),
+			})
+			.then((product) => {
+				if (req.files.length) {
+					let image = req.files.map(({ filename }) => {
+						return {
+							file: filename,
+							productId: product.id,
+						};
+					});
+					db.Image.bulkCreate(image, {
+						validate: true,
+					}).then((result) => console.log(result));
+				}
+				return res.redirect("/products");
+			})
+			.catch((error) => console.log(error));
+	},
+	editionProduct: (req, res) => {
+		db.Product.findByPk(req.params.id)
+			.then((product) => {
+				return res.render("editionProduct", {
+					product,
+				});
+			})
+			.catch((error) => console.log(error));
+	},
+	update: (req, res) => {
 		db.Product.update(
 			{
 				...req.body,
-				name : req.body.name.trim(),
-				description : req.body.description.trim()
+				name: req.body.name.trim(),
+				description: req.body.description.trim(),
 			},
 			{
-				where : {
-					id : req.params.id
-				}
+				where: {
+					id: req.params.id,
+				},
 			}
 		)
-		.them(() => res.redirect('/products/totalProducts'))
-		.catch(error => console.log(error))
-	},	
-    store: (req, res) => {
-		
-		//return res.send(req.file)
-		
-		/*const {name, price, code, color, description,caja, finish, style, dimensions, pei, transit, recomendation, espesor, model, form, origin} = req.body
-		const products = loadProducts();
-        
-		const newProduct ={
-			id : (products[products.length - 1].id + 1),
-			name : name?.trim(),
-			description : description?.trim(),
-			price : +price,
-			code : +code,
-			caja: +caja,
-			color: color?.trim(),
-			finish: finish?.trim(),
-			style: style?.trim(),
-			dimensions : +dimensions,
-			pei: +pei,
-			transit: transit?.trim(),
-			recomendation: recomendation?.trim(),
-			espesor: +espesor,
-			model: model?.trim(),
-			form: form?.trim(),
-			origin: origin?.trim(),
-			image : req.file ? req.file.filename: 'ceramica1.jpg',
-			
-		}
-		const productsModify = [...products, newProduct];
-
-		storeProducts(productsModify);
-
-		return res.redirect('/products/totalProducts') */
-		db.product.create({
-			...req.body,
-			name : req.body.trim(),
-			description : req.body.description.trim()
-		})
-		.then(product => {
-			if(req.files.length){
-				let image = req.files.map(({filename}) => {
-					return {
-						file : filename,
-						productId : product.id
-					}
-				})
-				db.Image.bulkCreate(image,{
-					validate : true
-				})
-				.then((result) =>console.log(result))
-			}
-			return res.redirect('/products')
-		})
-		.catch(error => console.log(error))
+			.them(() => res.redirect("/products/totalProducts"))
+			.catch((error) => console.log(error));
 	},
-    destroy : (req, res) => {
-		
-		/*const {id} = req.params;
-		const products = loadProducts();
-
-		const productModify = products.filter(product => product.id !== +id);
-
-		storeProducts(productModify); */
+	
+	destroy: (req, res) => {
+	
 		db.Product.destroy({
-			where : {
-				id : req.params.id
-			}
+			where: {
+				id: req.params.id,
+			},
 		})
-		.then(() => res.redirect('/products/totalProducts'))
-		.catch(error => console.log(error));
-	}
+			.then(() => res.redirect("/products/totalProducts"))
+			.catch((error) => console.log(error));
+	},
+	search: (req, res) => {
+		const { keywords } = req.query;
+		db.Product.findAll({
+			where: {
+				[Op.or]: [
+					{
+						name: {
+							[Op.substring]: keywords,
+						},
+					},
+					{
+						description: {
+							[Op.substring]: keywords,
+						},
+					},
+				],
+			},
+			include: ["image"],
+		})
+			.then((products) => {
+				returnres.render("results", {
+					products,
+					keywords,
+					toThousand,
+				});
+			})
+			.catch((error) => console.log(error));
+	},
+	productCart: (req, res) => {
+		return res.render("productCart", {
+			title: "ProductCart",
+		});
+	},
 };
-
 
 module.exports = controller;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,243 +1,146 @@
 const { validationResult } = require("express-validator");
-const { loadUsers, storeUsers } = require("../data/db");
-const {bcryptjs, compare} = require("bcryptjs");
+const { bcryptjs, compare } = require("bcryptjs");
 const path = require("path");
-const { literal } = require("sequelize");
 const db = require("../database/models");
 
 module.exports = {
-    login: (req, res) => {
-        return res.render('login', {
-            title: 'Login'
-        })
-    },processLogin: async (req, res) => {
-          const { email, password } = req.body;
-          if (!email || !password) {
-           return res.status(401).json({
-              ok: false,
-              status: 401,
-              msg: "El email y password son requeridos",
-            }); 
-          }
-    
-          //const user = await db.User.findOne({ where: { email } });
-    
-         
-    
-          /*if (!user) {
-            return res.status(404).json({
-              ok: false,
-              status: 404,
-              msg: "No existe ningún usuario con ese email",
-            }); 
-          }*/
-    
-          const userLogin = await db.User.findOne({ where: { email, password} });
+  register: (req, res) => {
+    return res.render("register", {
+      title: "Register",
+    });
+  },
+  processRegister: async (req, res) => {
+    const {
+      firstname,
+      lastname,
+      email,
+      password,
+      rolid,
+      avatar,
+      document,
+      nationality,
+      birthday,
+    } = req.body;
 
-          if (req.body.remember) {
-            res.cookie('ceramicas10', req.session.userLogin, {
-                maxAge: 1000 * 60
-            })
-        };
-        if(!userLogin){
-          return res.status(401).json({
-            ok: false,
-            status: 401,
-            msg: "Credenciales invalidas",
-          }); 
-        }
-        return res.redirect('/users/profile')
-          
-    },
-    /*processLogin: (req, res) => {
-        let errors = validationResult(req);
-        if (errors.isEmpty()) {
-            let { id, name, username, rol, avatar } = loadUsers().find(user => user.email === req.body.email);
-            req.session.userLogin = {
-                id,
-                username,
-                name,
-                rol,
-                avatar
-            };
-            if (req.body.remember) {
-                res.cookie('ceramicas10', req.session.userLogin, {
-                    maxAge: 1000 * 60
-                })
-            };
-            return res.redirect('/users/profile')
-        } else {
-            return res.render('login', {
-                title: 'Login',
-                errors: errors.mapped()
-            })
-        }
-    },*/
-    register: (req, res) => {
-        return res.render('register', {
-            title: 'Register'
-        })
-    },processRegister: async (req, res) =>{
-      const {firstname, lastname, email, password, rolid, avatar, document, nationality, birthday} = req.body
-      
-        const {id, rol_id} = await db.User.create({
-                firstname: firstname?.trim(),
-                lastname: lastname?.trim(),
-                email: email?.trim(),
-                password: password,
-                rolid: 2,
-                avatar: "defaultUser.png",
-                document: document,
-                nationality: nationality?.trim(),
-                birthday: birthday,
-        })
-        return res.redirect('/users/login')
+    const { id, rol_id } = await db.User.create({
+      firstname: firstname?.trim(),
+      lastname: lastname?.trim(),
+      email: email?.trim(),
+      password: password,
+      rolid: 2,
+      avatar: "defaultUser.png",
+      document: document,
+      nationality: nationality?.trim(),
+      birthday: birthday,
+    });
+    return res.redirect("/users/login");
+  },
+  login: (req, res) => {
+    return res.render("login", {
+      title: "Login",
+    });
+  },
+  processLogin: async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(401).json({
+        ok: false,
+        status: 401,
+        msg: "El email y password son requeridos",
+      });
     }
 
-    /*processRegister: (req, res) => {
-        let errors = validationResult(req);
+    const userLogin = await db.User.findOne({ where: { email, password } });
 
+    if (req.body.remember) {
+      res.cookie("ceramicas10", req.session.userLogin, {
+        maxAge: 1000 * 60,
+      });
+    }
+    if (!userLogin) {
+      return res.status(401).json({
+        ok: false,
+        status: 401,
+        msg: "Credenciales invalidas",
+      });
+    }
+    return res.redirect("/users/profile");
+  },
+  logout: async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const logoutUser = await db.User.destroy({
+        where: { id: userId },
+        force: true,
+      });
 
-        if (errors.isEmpty()) {
-            const { firstname, lastname, email, password } = req.body;
-            let users = loadUsers()
+      if (!logoutUser) {
+        return res.status(404).json({
+          ok: false,
+          status: 404,
+          msg: "Es probable que el usuario no existe",
+        });
+      }
 
-            let newUser = {
-                id: users.length ? users[users.length - 1].id + 1 : 1,
-                firstname: firstname?.trim(),
-                lastname: lastname?.trim(),
-                email: email?.trim(),
-                password: bcryptjs.hashSync(password, 12),
-                rol: 'user',
-                avatar: null,
-                document: null,
-                nationality: null,
-                birthday: null,
-            }
-            let usersModify = [...users, newUser];
-            storeUsers(usersModify)
-
-            return res.redirect('/users/login')
-        } else {
-            return res.render('register', {
-                title: 'Register',
-                errors: errors.mapped(),
-                old: req.body
-            })
-        }*/
-    
-
-    ,logout: async (req, res) => {
-        try {
-          const userId = req.params.id;
-          const logoutUser = await db.User.destroy({ where: { id: userId },force:true  });
-          
-          if (!logoutUser) {
-            return res.status(404).json({
-              ok: false,
-              status: 404,
-              msg: "Es probable que el usuario no existe",
-            });
-          }
-    
-          return res.status(200).json({
-            ok: true,
-            status: 200,
-          });
-    
-        } catch (error) {
-          res.status(500).json({
-            ok: false,
-            status: 500,
-            msg: error.message || "Server error",
-          });
-        }
-        return res.redirect('/')
-      },
-    /*,logout: (req, res) => {
-        res.session.destroy();
-        return res.redirect('/')*/
-
-    profile: (req, res) => {
-        let user = db.User.findOne({where : user.id === req.session.userLogin.id })
-        return res.render('profile', {
-            title: 'Profile',
-            user
-        })
-
-    },update: async (req, res) => {
-        const { id } = req.params.id;
-        const { firstname, lastname, avatar, document, nationality, birthday} = req.body;
-        try {
-          const options = {
-            include: [
-              {
-                attributes: {
-                  exclude: ["userId", "deletedAt"],
-                },
-              },
-            ],
+      return res.status(200).json({
+        ok: true,
+        status: 200,
+      });
+    } catch (error) {
+      res.status(500).json({
+        ok: false,
+        status: 500,
+        msg: error.message || "Server error",
+      });
+    }
+    return res.redirect("/");
+  },
+  profile: (req, res) => {
+    let user = db.User.findOne({ where: user.id === req.session.userLogin.id });
+    return res.render("profile", {
+      title: "Profile",
+      user,
+    });
+  },
+  update: async (req, res) => {
+    const { id } = req.params.id;
+    const { firstname, lastname, avatar, document, nationality, birthday } =
+      req.body;
+    try {
+      const options = {
+        include: [
+          {
             attributes: {
-              exclude: ["deletedAt", "password"],       
+              exclude: ["userId", "deletedAt"],
             },
-          };
-          const user = await db.User.findByPk(id, options);
-    
-          user.firstname = firstname?.trim() || user.firstname;
-          user.lastname = lastname?.trim() || user.lastname;
-          user.avatar = req.file?.filename || user.avatar;
-          user.document = document?.trim() || user.document;
-          user.nationality = nationality?.trim() || user.nationality;
-          user.birthday = birthday?.trim() || user.birthday;
-    
-          await user.save();
-    
-          return res.status(200).json({
-            ok: true,
-            status: 200,
-            data: user,
-          });
-        } catch (error) {
-          res.status(500).json({
-            ok: false,
-            status: 500,
-            msg: error.message || "Ocurrió un error",
-          });
-        }
-    /*update: (req, res) => {
-         OBTENEMOS LOS USUARIOS 
-        const users = loadUsers()
-        /* DESTRUCTURACION DEL BODY 
-        const { firstname,
-            lastname,
-            nationality,
-            document,
-            gender,
-            birthday } = req.body
+          },
+        ],
+        attributes: {
+          exclude: ["deletedAt", "password"],
+        },
+      };
+      const user = await db.User.findByPk(id, options);
 
-        /* MAPEAMOS LOS USUARIOS 
-        const usersModify = users.map(user => {
+      user.firstname = firstname?.trim() || user.firstname;
+      user.lastname = lastname?.trim() || user.lastname;
+      user.avatar = req.file?.filename || user.avatar;
+      user.document = document?.trim() || user.document;
+      user.nationality = nationality?.trim() || user.nationality;
+      user.birthday = birthday?.trim() || user.birthday;
 
-            /* SI EL ID DEL USUARIO QUE SE RECORRE ES IGUAL AL PARAMETRO QUE MANDAMOS POR LA URL 
-            if (user.id === +req.params.id) {
-                /* RETORORNAMOS EL USUARIO QUE COINCIDE Y LE ACTUALIZAMOS LOS DATOS A PARTIR DE LO QUE NOS VIENE POR EL BODY 
-                return {
-                    ...user,
-                    firstname: firstname?.trim(),
-                    lastname: lastname?.trim(),
-                    nationality: nationality?.trim(),
-                    document: document?.trim(),
-                    gender: gender?.trim(),
-                    birthday,
-                    avatar:null
-                }
-            }
-            return user
-        })
-        storeUsers(usersModify)
-        res.redirect("/users/profile")
-        */
+      await user.save();
+
+      return res.status(200).json({
+        ok: true,
+        status: 200,
+        data: user,
+      });
+    } catch (error) {
+      res.status(500).json({
+        ok: false,
+        status: 500,
+        msg: error.message || "Ocurrió un error",
+      });
     }
-
-
-  }
+  },
+};
