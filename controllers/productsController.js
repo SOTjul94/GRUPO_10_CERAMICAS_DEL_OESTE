76e50d81846007d99dfd1dc204cc1a7004afd39d
SOTjul94 <lyweonardo@hotmail.com>
2022-11-14 19:07:35 +0100

    product-edit

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,52 +1,7 @@
 const { Op } = require("sequelize");
 const db = require("../database/models");
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
 const controller = {
-	
-	// Create - Form to create
-	create: (req, res) => {
-		// Do the magic
-		db.Category.findAll({
-			attributes : ['id','name'],
-			order : ['name']
-		})
-			.then(categories => {
-				return res.render('product-create-form', {
-					categories
-				})
-			})
-			.catch(error => console.log(error))
-	},
-	/////////////// leo ////////////////////////////
-	// Create - store
-	store: (req, res) => {
-		// Do the magic
-		db.Product.create({
-			...req.body,
-			name : req.body.name.trim(),
-			description : req.body.description.trim()
-		})
-			.then(product => {
-				if(req.files.length){
-					let images = req.files.map(({filename}) => {
-						return {
-							file : filename,
-							productId: product.id
-						}
-					})
-					db.Image.bulkCreate(images,{
-						validate : true
-					}).then( (result) => console.log(result) )
-				}
-				return res.redirect('/products')
-			})
-			.catch(error => console(error))
-	},
-	/////////////////create-store ////leo///////////////
-
-	
-	
 	totalProducts: (req, res) => {
 		db.Product.findAll({
 			include: ["images"],
@@ -88,13 +43,138 @@ const controller = {
 		return res.render("creationProduct");
 	},
 	store: (req, res) => {
-
-/////////
-
+		
 		/* ************************** */
-		/* HACER VALIDACIONES BACKEND */
+		/* HACER VALIDACIONES BACKEND *////
+		///////////////////////////////
+		const controller = {
+			// Root - Show all products
+			index: (req, res) => {
+				// Do the magic
+			
+				db.Product.findAll({
+					include : ['images']
+				})
+					.then(products => res.render('products', {
+						products,
+						toThousand
+					}))
+			},
+		
+			// Detail - Detail from one product
+			detail: (req, res) => {
+				// Do the magic
+				db.Product.findByPk(req.params.id,{
+					include : ['images']
+				})
+					.then(product => res. render('detail', {
+						product,
+						toThousand
+					}))
+					.catch(error => console.log(error))
+			},
+		
+			// Create - Form to create
+			create: (req, res) => {
+				// Do the magic
+				db.Category.findAll({
+					attributes : ['id','name'],
+					order : ['name']
+				})
+					.then(categories => {
+						return res.render('product-create-form', {
+							categories
+						})
+					})
+					.catch(error => console.log(error))
+			},
+			
+			// Create -  Method to store
+			store: (req, res) => {
+				// Do the magic
+				db.Product.create({
+					...req.body,
+					name : req.body.name.trim(),
+					description : req.body.description.trim()
+				})
+					.then(product => {
+						if(req.files.length){
+							let images = req.files.map(({filename}) => {
+								return {
+									file : filename,
+									productId: product.id
+								}
+							})
+							db.Image.bulkCreate(images,{
+								validate : true
+							}).then( (result) => console.log(result) )
+						}
+						return res.redirect('/products')
+					})
+					.catch(error => console(error))
+			},
+		
+			// Update - Form to edit
+			edit: (req, res) => {
+				// Do the magic
+				let categories = db.Category.findAll({
+					attributes : ['id','name'],
+					order : ['name']
+				});
+				let product = db.Product.findByPk(req.params.id);
+		
+				Promise.all([categories,product])
+					.then(([categories,product]) => {
+						return res.render('product-edit-form',{
+							product,
+							categories
+						})
+					})
+					.catch(error => console.log(error));
+			},
+			// Update - Method to update
+			update: (req, res) => {
+				// Do the magic
+				db.Product.update(
+					{
+						...req.body,
+						name : req.body.name.trim(),
+						description : req.body.description.trim()
+					},
+					{
+						where : {
+							id : req.params.id
+						}
+					}
+				)
+					.then( () => res.redirect('/products/detail/' + req.params.id) )
+					.catch(error => console.log(error))
+				 
+			},
+		
+			// Delete - Delete one product from DB
+			destroy : (req, res) => {
+				// Do the magic
+			
+				db.Product.destroy({
+					where : {
+						id : req.params.id
+					}
+				})
+					.then( () => res.redirect('/products'))
+					.catch( error => console.log(error));
+			}
+		};
+		
+	/////////HACER VALIDACIONES BACKEND///////7777
 		/* ************************** */
 
+		
+		
+		
+		
+		
+		
 		const {name, model, price, box, discount, description, color, style, dimension, transit, origin ,pei, recomendation, code, category} = req.body
 
 		db.Product.create({
