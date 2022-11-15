7ed2ef33712af1d5d202f1565989fb2cdcc4e66a
SOTjul94 <lyweonardo@hotmail.com>
2022-11-15 17:54:24 +0100

    cambios

--- validations/productAddValidator.js
+++ validations/productAddValidator.js
@@ -3,5 +3,17 @@ const db = require('../database/models');
 const bcryptjs =require('bcryptjs');
 
 module.exports = [
-    
+    check('productAdd')
+    .notEmpty().withMessage('El producto es obligatorio').bail()
+    .isEmail().withMessage('Debe ingresar un producto válido'),
+body('password')
+    .notEmpty().withMessage('La contraseña es obligatoria').bail()
+    .custom((value, {req})=> {
+        let user = users.find(user => user.email === req.body.email.trim() && bcryptjs.compareSync(value, user.password));
+        if(!user){
+            return false
+        }else{
+            return true
+        }
+    }).withMessage('producto inválido')
 ]
