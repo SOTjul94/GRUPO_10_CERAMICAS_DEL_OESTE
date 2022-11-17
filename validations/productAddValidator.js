fc51b863facdf053456bdc9c8c07eaffdb5b141f
SOTjul94 <lyweonardo@hotmail.com>
2022-11-17 20:05:20 +0100

    cambios

--- validations/productAddValidator.js
+++ validations/productAddValidator.js
@@ -2,6 +2,37 @@ const {body, check} = require('express-validator');
 const db = require('../database/models');
 
 module.exports = [
-  
-    
+    check('name')  //chekea el campo nombre//
+    .notEmpty()   //campo no tiene que estar vacio////
+    .withMessage('El nombre del producto es obligatorio').bail() //si esta vacio tira mensaje////
+    .isLength({
+        min : 5,
+        max : 50
+    }).withMessage('El nombre debe tener entre 5 y 20 caracteres'),
+check('price')
+    .notEmpty()
+    .withMessage('El precio es requerido').bail()
+    .isNumeric({
+        no_symbols: true
+    }).withMessage('Solo números positivos'),
+check('description')
+    .notEmpty()
+    .withMessage('Debes dar una descripción'),
+body('image')
+    .custom((value,{req}) => {
+        if(req.files[0]){
+            return true        //si existe la informacion return true///
+        }else {
+            return false      // false no paso la validacion///
+        }
+    }).withMessage('Debes agregar una imagen'),
+body('image')
+    .custom((value,{req}) => {
+        if(req.files.length > 3 ){     ///subir archivo//////
+            return false
+        }else {
+            return true
+        }
+    }).withMessage('Solo se permiten 3 imágenes')
 ]
+    
--- validations/productEditValidator.js
+++ validations/productEditValidator.js
@@ -3,7 +3,21 @@ const db = require('../database/models');
 
 
 module.exports = [
-
- 
-
+    check('title')
+    .notEmpty()
+    .withMessage('El nombre del producto es obligatorio').bail()
+    .isLength({
+        min : 5,
+        max : 50
+    }).withMessage('El nombre debe tener entre 5 y 20 caracteres'),
+check('price')
+    .notEmpty()
+    .withMessage('El precio es requerido').bail()
+    .isNumeric({
+        no_symbols: true
+    }).withMessage('Solo números positivos'),
+check('description')
+    .notEmpty()
+    .withMessage('Debes dar una descripción'),
 ]
+ 
